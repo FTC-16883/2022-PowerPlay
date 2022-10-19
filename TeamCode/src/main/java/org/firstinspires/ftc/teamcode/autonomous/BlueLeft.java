8e33824217797bd5a5c24a30a2e0b16ada46a260
AayushLokhande <113805011+AayushLokhande@users.noreply.github.com>
2022-10-19 18:59:01 -0400

    Added arm code
    Added arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueLeft.java
@@ -6,8 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Arm;
 import org.firstinspires.ftc.teamcode.Drivetrain;
 
 /* Copyright (c) 2017 FIRST. All rights reserved.
@@ -106,6 +108,82 @@ public class BlueLeft extends LinearOpMode
         Drivetrain.encoderForward(-24);
         Drivetrain.encoderStrafe(-24);
 
+
+         class Arm {
+            public DcMotorEx arm;
+            public  Servo clawLeft;
+            public  Servo clawRight;
+
+            public Arm() {
+
+            }
+
+            public static void init(DcMotorEx arm, Servo clawLeft, Servo clawRight) {
+                org.firstinspires.ftc.teamcode.Arm.arm = arm;
+                org.firstinspires.ftc.teamcode.Arm.clawLeft = clawLeft;
+                org.firstinspires.ftc.teamcode.Arm.clawRight = clawRight;
+
+                clawRight.setDirection(Servo.Direction.REVERSE);
+            }
+
+            public static void openClaw() {
+                clawLeft.setPosition(0);
+                clawRight.setPosition(0);
+            }
+
+            public static void closeClaw() {
+                clawLeft.setPosition(1);
+                clawRight.setPosition(1);
+            }
+
+            public static void armFloor() {
+                arm.setTargetPosition(0);
+
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+                arm.setPower(1);
+
+                while (arm.isBusy()) {
+                    //Telemetry.updateArmEncoder();
+                }
+            }
+
+            public static void armLow() {
+                arm.setTargetPosition(1000);
+
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+                arm.setPower(1);
+
+                while (arm.isBusy()) {
+                    //Telemetry.updateArmEncoder();
+                }
+            }
+
+            public static void armMedium() {
+                arm.setTargetPosition(2000);
+
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+                arm.setPower(1);
+
+                while (arm.isBusy()) {
+                    //Telemetry.updateArmEncoder();
+                }
+            }
+
+            public static void armHigh() {
+                arm.setTargetPosition(3000);
+
+                arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+                arm.setPower(1);
+
+                while (arm.isBusy()) {
+                    //Telemetry.updateArmEncoder();
+                }
+            }
+        }
 
 
     }
