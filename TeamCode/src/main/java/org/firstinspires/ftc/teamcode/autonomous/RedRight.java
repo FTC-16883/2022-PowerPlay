e5458b417ec54bc45f4f54999d3fe701331e7e79
Simen7 <sme7n@msn.com>
2022-11-09 19:06:01 -0500

    Update Autonomous Arm/Wrist
    Add Logic to drop cone on Medium Junction.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedRight.java
@@ -111,12 +111,12 @@ public class RedRight extends LinearOpMode
 
         waitForStart();
         //grip cone for autonomous
-        Arm.wristLevel();
+        //Arm.wristLevel();
 
         Arm.closeClaw();
         sleep(2000);
         Arm.wristIn();
-
+        sleep(1000);
         //Siddharth M Trials:
         if ((camInput.color1average>150)) {
             locSignal = 3;
@@ -125,7 +125,7 @@ public class RedRight extends LinearOpMode
             locSignal = 1;
             telemetry.addData("Detected color is green :", 1);
         }
-        else if ((camInput.color2average>125)&&(camInput.color2average<150)){
+        else if ((camInput.color2average>125)&&(camInput.color2average<140)){
             locSignal = 2;
             telemetry.addData("Detected color is yellow :", 2);
         }
@@ -137,19 +137,24 @@ public class RedRight extends LinearOpMode
             telemetry.update();
 
         camInput.webcam.pauseViewport();// Pause image for processing
-        Drivetrain.encoderForward(46);
+        Drivetrain.encoderForward(50);
         Arm.armMedium();
+        sleep(500);
+        Drivetrain.encoderTurn(-95);
         sleep(1000);
-        Drivetrain.encoderTurn(90);
-        sleep(1000);
+
         Arm.wristScore();
         sleep(1000);
         Arm.openClaw();
         sleep(1000);
         Arm.closeClaw();
-        sleep(1000);
+        sleep(500);
         //Drivetrain.forward(-0.5);
-        Drivetrain.encoderTurn(90);
+        Drivetrain.encoderTurn(-95);
+        sleep(500);
+        Arm.armFloor();
+        sleep(500);
+        Arm.wristIn();
         Drivetrain.encoderForward(13);
         if (locSignal == 2){
             sleep(1000);
