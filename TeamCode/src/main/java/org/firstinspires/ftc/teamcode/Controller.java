f1d75a2f51ee0616c3e7a61d93a3bd5ccd2fe4fb
MokshithAddala28 <113805119+MokshithAddala28@users.noreply.github.com>
2023-09-30 11:50:07 -0400

    teleoo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
@@ -14,12 +14,15 @@ public class Controller extends LinearOpMode {
     public static DcMotorEx leftRear;
     public static DcMotorEx rightRear;
 
+    public static DcMotorEx droneLauncher;
+
     @Override
     public void runOpMode() throws InterruptedException {
         leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
         rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        droneLauncher = hardwareMap.get(DcMotorEx.class, "droneLauncher");
 
         Drivetrain.init(leftFront, rightFront, leftRear, rightRear);
 
@@ -30,6 +33,7 @@ public class Controller extends LinearOpMode {
             rightFront.setPower((gamepad1.left_stick_y) + (gamepad1.right_stick_x) + (gamepad1.left_stick_x));
             leftRear.setPower((gamepad1.left_stick_y) + (gamepad1.right_stick_x) + (-gamepad1.left_stick_x));
             rightRear.setPower((gamepad1.left_stick_y) + (-gamepad1.right_stick_x) + (gamepad1.left_stick_x));
+            droneLauncher.setPower(gamepad1.triangle);
             // Makes gamepad1 sticks correlate to different functions. like strafing, turning and going forward
 
         }
