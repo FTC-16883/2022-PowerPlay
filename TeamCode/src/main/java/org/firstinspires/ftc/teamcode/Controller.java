93aed5db5b5e13b43367bed708647253ed9fac04
MokshithAddala28 <113805119+MokshithAddala28@users.noreply.github.com>
2023-10-05 18:31:36 -0400

    Teleop
    coded the arm and the arm extender

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -15,8 +17,13 @@ public class Controller extends LinearOpMode {
     public static DcMotorEx rightFront;
     public static DcMotorEx leftRear;
     public static DcMotorEx rightRear;
-
+    public static DcMotorEx leftRobotArm;
+    public static DcMotorEx rightRobotArm;
+    public static DcMotorEx armExtender;
     public static DcMotorEx droneLauncher;
+
+
+    
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,6 +31,11 @@ public class Controller extends LinearOpMode {
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
         rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        leftRobotArm = hardwareMap.get(DcMotorEx.class, "leftRobotArm");
+        leftRobotArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRobotArm = hardwareMap.get(DcMotorEx.class, "rightRobotArm");
+        rightRobotArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armExtender = hardwareMap.get(DcMotorEx.class, "armExtender");
         droneLauncher = hardwareMap.get(DcMotorEx.class, "droneLauncher");
 
         Drivetrain.init(leftFront, rightFront, leftRear, rightRear);
@@ -31,12 +43,24 @@ public class Controller extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+
             leftFront.setPower((-gamepad1.left_stick_y) + (gamepad1.right_stick_x) + (gamepad1.left_stick_x));
             rightFront.setPower((gamepad1.left_stick_y) + (gamepad1.right_stick_x) + (gamepad1.left_stick_x));
             leftRear.setPower((gamepad1.left_stick_y) + (gamepad1.right_stick_x) + (-gamepad1.left_stick_x));
             rightRear.setPower((gamepad1.left_stick_y) + (-gamepad1.right_stick_x) + (gamepad1.left_stick_x));
-            droneLauncher.setPower(gamepad1.right_trigger);
-            // Makes gamepad1 sticks correlate to different functions. like strafing, turning and going forward
+            if (gamepad1.triangle) {
+                droneLauncher.setPower(1);
+            } else {
+                droneLauncher.setPower(0);
+            }
+            droneLauncher.setPower(gamepad1.left_trigger);
+            leftRobotArm.setPower(gamepad2.left_stick_y);
+            rightRobotArm.setPower(-gamepad2.left_stick_y);
+            armExtender.setPower(-gamepad2.right_stick_y);
+
+
+            //Gamepad1 sticks correlate to different functions. like strafing, turning and going forward
+
 
         }
 
